5d3192e25298f49a9d16510728fad017d5638cc2
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-06-25 22:30:30 -0300

    Cartoes dos Contatos

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_agenda_contatos/helpers/contact_helper.dart';
 
@@ -40,8 +42,50 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(padding: EdgeInsets.all(10.0),
           itemCount: contacts.length,
           itemBuilder: (context,index){
-          
+            return _contactCard(context, index);
       }),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector( //usamos porque o card não permite evento de clique
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: contacts[index].img != null ?
+                      FileImage(File(contacts[index].img)) :
+                      AssetImage("images/person.png"),
+                    fit: BoxFit.cover
+                  )
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(contacts[index].name ?? "",
+                      style: TextStyle(fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                      Text(contacts[index].email ?? "",
+                        style: TextStyle(fontSize: 18.0)),
+                      Text(contacts[index].phone ?? "",
+                        style: TextStyle(fontSize: 18.0))
+                    ],
+                  ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
